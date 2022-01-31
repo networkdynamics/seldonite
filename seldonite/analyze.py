01a9052ba3755346e32cf1a276fb8d6a7d7c6137
Delal Tomruk <delaltomruk@gmail.com>
2022-01-31 10:19:30 -0500

    Update analyze.py

--- seldonite/analyze.py
+++ seldonite/analyze.py
@@ -4,6 +4,9 @@ import pyspark.sql as psql
 
 from seldonite import collect
 
+from geotext import GeoText
+from geopy import geocoders
+
 class Analyze():
     collector: collect.Collector
 
@@ -23,4 +26,37 @@ class Analyze():
 
             #df.groupby
 
-        return df
+            return df
+
+
+        def proportion_of_countries(self):
+            self.collector._check_args()
+
+            spark_builder = self.collector._get_spark_builder()
+
+            with spark_builder.start_session() as spark_manager:
+                df = self.collector._fetch(spark_manager)
+
+                df = df.withColumn('all_text', psql.functions.concat(df['headline'], psql.functions.lit(' '), df['short_description']))
+
+                locations = []   
+
+                for i in df.rdd.collect():
+                    places = GeoText(i.all_text)
+                    if not places.cities:
+                            locations.append(['missing'])
+                    else:
+                        locations.append(places.cities)
+
+                with_country = []
+
+                for i in locations:
+                    if i == ['missing']:
+                        with_country.append("missing")
+                    else:
+                        locator = geopy.geocoders.Nominatim(user_agent="MyCoder")
+                        location = locator.geocode(i)
+                        country = str(location).rsplit(', ', 1)[1]
+                        with_country.append(country)
+
+            return dict((i, with_country.count(i)) for i in with_country)
