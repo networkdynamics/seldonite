ac71332dbdea3c5733dd0068c4925217de154de6
Ben Steel <bendavidsteel@gmail.com>
2022-04-28 16:00:28 -0400

    Making example script for country filter

---
+++ examples/collect/collect_countries.py
@@ -0,0 +1,24 @@
+import argparse
+
+from seldonite import collect, run, nlp
+from seldonite.sources import news
+
+def main(args):
+
+    csv_source = news.CSV(args.input)
+    collector = collect.Collector(csv_source) \
+        .mentions_countries(countries=['US'], min_num_countries=2)
+
+    runner = run.Runner(collector) 
+    df = runner.to_pandas()
+
+    df.to_csv(args.output)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input')
+    parser.add_argument('-o', '--output')
+    args = parser.parse_args()
+
+    main(args)
